7bad8299724475e2ab1f8fb0263350590adcef82
lyalan <32605200+lyalan@users.noreply.github.com>
2018-12-18 11:29:16 +0800

    Add files via upload

---
+++ 实验十二/实验十二/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  实验十二
+//
+//  Created by student on 2018/12/17.
+//  Copyright © 2018年 刘敏琦. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var person:Person?
+    
+    @IBOutlet weak var stuNoLB: UILabel!
+    @IBOutlet weak var stuNameLB: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stuNoLB.text = person?.stuNo
+        stuNameLB.text = person?.stuName
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
